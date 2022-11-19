f801f9053603f2b63dd30bbadd208312b0f789e4
Vashdi <Vashdi7002@gmail.com>
2022-11-19 22:51:13 +0200

    fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,3 @@
-export const checkIfNeedsDisable = (size, length) => {
-  return size >= length;
-};
-
 export const createInfo = (fullName, title, family) => {
   const nameAndTitleString = `${fullName}, ${title}`;
   const houseString = `<br />house: ${family}`;
